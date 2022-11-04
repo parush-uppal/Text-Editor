3e723f068c50563a5de9a87466e6a63f02c9ecaa
parush-uppal <parush1008.be21@chitkara.edu.in>
2022-11-04 13:38:05 +0530

    test

--- src/About.js
+++ src/About.js
@@ -6,6 +6,7 @@ export default function About(props) {
     //     color: 'black',
     //     backgroundColor: 'white'
     // }) 
+    /// Just Testing Something
     let myStyle = {
         color: props.mode ==='dark'?'white':'#042743',
         backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
